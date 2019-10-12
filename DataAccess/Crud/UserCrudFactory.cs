540424e7a84148037a113b1ec97656598a4d46cb
dsolanof <dsolanof@ucenfotec.ac.cr>
2019-10-12 10:50:56 -0600

    finish with crud´s logic

--- DataAccess/Crud/UserCrudFactory.cs
+++ DataAccess/Crud/UserCrudFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccess.Dao;
+using DataAccess.Mapper;
+using Entities_POJO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,67 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Crud
 {
-    class UserCrudFactory
+    public class UserCrudFactory: CrudFactory
     {
+        UserMapper mapper;
+
+        public UserCrudFactory() : base()
+        {
+            mapper = new UserMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseEntity entity)
+        {
+            var account = (User)entity;
+            var sqlOperation = mapper.GetCreateStatement(account);
+            dao.ExecuteProcedure(sqlOperation);
+        }
+
+
+
+        public override T Retrieve<T>(BaseEntity entity)
+        {
+            var lstResult = dao.ExecuteQueryProcedure(mapper.GetRetriveStatement(entity));
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                dic = lstResult[0];
+                var objs = mapper.BuildObject(dic);
+                return (T)Convert.ChangeType(objs, typeof(T));
+            }
+
+            return default(T);
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            var lstAccounts = new List<T>();
+
+            var lstResult = dao.ExecuteQueryProcedure(mapper.GetRetriveAllStatement());
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                var objs = mapper.BuildObjects(lstResult);
+                foreach (var c in objs)
+                {
+                    lstAccounts.Add((T)Convert.ChangeType(c, typeof(T)));
+                }
+            }
+
+            return lstAccounts;
+        }
+
+        public override void Update(BaseEntity entity)
+        {
+            var account = (User)entity;
+            dao.ExecuteProcedure(mapper.GetUpdateStatement(account));
+        }
+
+        public override void Delete(BaseEntity entity)
+        {
+            var account = (User)entity;
+            dao.ExecuteProcedure(mapper.GetDeleteStatement(account));
+        }
     }
 }
